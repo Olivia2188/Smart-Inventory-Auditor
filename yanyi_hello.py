b0528aa1a36da0a7affcfbdc7bc3ad79ee8aff0c
Yan Yi <oliviawongyy@gmail.com>
2026-09-04 11:37:48 +0800

    activity 2

--- yanyi_hello.py
+++ yanyi_hello.py
@@ -3,3 +3,12 @@ print("==========================================")
 print("Welcome here")
 print("My first post!")
 print("==========================================")
+
+# Activity 2
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
